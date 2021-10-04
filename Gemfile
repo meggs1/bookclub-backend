f1ca59a12c5d40affb6e4eaf40c4c336bde5ea0a
megan <megan.schertz@gmail.com>
2021-10-04 14:40:26 -0500

    add .env and jwt secret key

--- Gemfile
+++ Gemfile
@@ -41,3 +41,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "jwt", "~> 2.3"
+
+gem "dotenv", "~> 2.7"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  skip_before_action :authorized, only: [:create]
+
 
   # GET /users
   def index
@@ -13,13 +15,19 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-  # POST /users
+
+  def profile
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+  
+   # POST /users
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
-      render json: { error: @user.errors }, status: :unprocessable_entity
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
     end
   end
 
